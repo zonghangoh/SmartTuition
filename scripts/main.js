97434eb67d41bbd237226c32e213536e40fa27c0
Mingcong <mli014@e.ntu.edu.sg>
2021-10-28 15:08:10 +0800

    footer version two

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+/* For clicking collapsible menu */
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+/* End */
